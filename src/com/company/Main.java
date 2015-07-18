9cefc6ce53a1020eb5cf415321b39f8f12fca3ce
zinkovsky <zinkovsky.m@gmail.com>
2015-07-19 00:31:54 +0300

    change

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,14 @@
 package com.company;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 public class Main {
 
     public static void main(String[] args) {
         long lg = Long.MAX_VALUE;
         byte b = Byte.MAX_VALUE;
         System.out.println(b);
+
+        System.out.println(lg);
     }
 }
