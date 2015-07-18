066ea479d353e3a84e7b5ef43b501e967ea882ab
zinkovsky <zinkovsky.m@gmail.com>
2015-07-19 01:07:32 +0300

    my first project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,19 @@
 package com.company;
 
-public class Main {
+public class Main
+    {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+        {
         int a = 10;
         int b = 20;
 
-        if (a < b) {
+        if (a < b)
+            {
             System.out.println("pizdezsh");
-        } else {
+            } else
+            {
             System.out.println("true!!!");
+            }
         }
     }
-}
