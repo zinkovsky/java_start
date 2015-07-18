2016e4f4db76598332d8edc04650d34b2731d87a
zinkovsky <zinkovsky.m@gmail.com>
2015-07-19 00:07:48 +0300

    lesson 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        long lg = Long.MAX_VALUE;
+        byte b = Byte.MAX_VALUE;
+        System.out.println(b);
+    }
+}
