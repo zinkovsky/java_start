9bc4ba48795e71cc38bd236927ec2b5ef9f1d321
zinkovsky <zinkovsky.m@gmail.com>
2015-07-19 20:58:02 +0300

    lesson 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String s1 = "привет";
+        s1.valu
+        char ch = s1.charAt(0);
+        System.out.println(ch);
+    }
+}
